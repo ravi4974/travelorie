438944f96f0a1a6d4b9ce30633cbf1e5c11a676e
Ravi <expertravi2008@gmail.com>
2020-04-07 00:39:39 +0200

    Build initial setup

---
+++ precache-manifest.9858a255495138748efd133575530981.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5957378598d6e30c15d2b3cec8eddf31",
+    "url": "/travelorie/index.html"
+  },
+  {
+    "revision": "d6c6ecc6b27bb4448e3c",
+    "url": "/travelorie/static/css/main.818745ed.chunk.css"
+  },
+  {
+    "revision": "15af48d6f28c3610962b",
+    "url": "/travelorie/static/js/2.aa89d58d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/travelorie/static/js/2.aa89d58d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6c6ecc6b27bb4448e3c",
+    "url": "/travelorie/static/js/main.69455928.chunk.js"
+  },
+  {
+    "revision": "cd2ff6272c523d90aaa4",
+    "url": "/travelorie/static/js/runtime-main.3969994d.js"
+  },
+  {
+    "revision": "7eeb294c3f07f2fc6a3e03ecdbba380a",
+    "url": "/travelorie/static/media/arrow_left.7eeb294c.svg"
+  },
+  {
+    "revision": "7917dd3ef5de6ee4a3d2152fff64015b",
+    "url": "/travelorie/static/media/arrow_right.7917dd3e.svg"
+  }
+]);
